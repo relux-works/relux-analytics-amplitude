e9d970bb0d28c08ec255948d201a09564fafd673
Ivan Oparin <i.oparin@unitea.com>
2024-09-04 21:28:08 +0400

    Switch to spm-bug-free fork of amplitude

--- Package.swift
+++ Package.swift
@@ -14,14 +14,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "git@github.com:ivalx1s/relux-analytics.git", from: "1.0.0"),
-        .package(url: "git@github.com:amplitude/Amplitude-iOS.git", from: "8.21.0"),
+        .package(url: "git@github.com:ivalx1s/dawrin-amplitude.git", from: "8.14.010"),
     ],
     targets: [
         .target(
             name: "ReluxAnalyticsAmplitude",
             dependencies: [
                 .product(name: "ReluxAnalytics", package: "relux-analytics"),
-                .product(name: "Amplitude", package: "Amplitude-iOS")
+                .product(name: "Amplitude", package: "dawrin-amplitude")
             ]
         )
     ]
